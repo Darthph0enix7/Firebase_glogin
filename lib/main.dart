c37a29e2067910705639abe2a835d72622a9aeb1
Darthph0enix7 <kalinsaz123321@gmail.com>
2024-04-21 18:57:43 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,92 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:to_do_app/res/routes/app_routes.dart';
-import 'package:to_do_app/view/sign%20up/sign_up.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      getPages: AppRoutes.routes(),
+    return MaterialApp(
+      home: LoginPage(),
     );
   }
 }
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _handleSignIn() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      if (userCredential.user != null) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => MainScreen()));
+      }
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error signing in: $error'),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Google Sign-In")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _handleSignIn,
+          child: Text("Sign in with Google"),
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                // Code to see files
+              },
+              child: Text("See Files"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Code to upload files
+              },
+              child: Text("Upload Files"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/res/routes/routes.dart
+++
@@ -1,6 +0,0 @@
-class Routes{
-  static const splashScreen='/';
-  static const signUpScreen='/signUpScreen';
-  static const signInScreen='/signInScreen';
-  static const homePage='/homePage';
-}
