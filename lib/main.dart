78ce68720c626214297b6e94f122f522c634c470
Darthph0enix7 <kalinsaz123321@gmail.com>
2024-04-23 21:57:18 +0200

    drive first integration

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,20 +27,34 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    // Scopes for Google Drive access, adjust scopes based on your needs
+    scopes: [
+      drive.DriveApi.driveFileScope,
+    ],
+  );
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> _handleSignIn() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
+
       final UserCredential userCredential = await _auth.signInWithCredential(credential);
       if (userCredential.user != null) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => MainScreen()));
+        // Here we get the auth headers needed for the Google Drive API
+        final authHeaders = await googleUser.authHeaders;
+        final authenticateClient = GoogleAuthClient(authHeaders);
+        final driveApi = drive.DriveApi(authenticateClient);
+
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (_) => MainScreen(driveApi: driveApi),
+        ));
       }
     } catch (error) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -60,7 +77,22 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+class GoogleAuthClient extends http.BaseClient {
+  final Map<String, String> _headers;
+  final http.Client _client = http.Client();
+
+  GoogleAuthClient(this._headers);
+
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    return _client.send(request..headers.addAll(_headers));
+  }
+}
+
 class MainScreen extends StatelessWidget {
+  final drive.DriveApi driveApi;
+
+  MainScreen({required this.driveApi});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,15 +104,28 @@ class MainScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: () {
-                // Code to see files
+              onPressed: () async {
+                // Here you can list files, for example
+                try {
+                  var files = await driveApi.files.list(spaces: 'drive');
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Text("Files: ${files.files?.map((f) => f.name).join(", ") ?? "No files found"}"),
+                    );
+                  },
+                );
+                } catch (e) {
+                  print('Error listing files: $e');
+                }
               },
               child: Text("See Files"),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Code to upload files
+                // Here you could handle file uploads
               },
               child: Text("Upload Files"),
             ),
